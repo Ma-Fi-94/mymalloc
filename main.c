49ec00ad0fac4dea8625b28a1808e1c0f5929ac7
Ma-Fi-94 <33027321+Ma-Fi-94@users.noreply.github.com>
2022-02-27 02:55:32 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -18,8 +18,49 @@ struct metadata {
 #define META_SIZE sizeof(struct metadata)
 
 
-// Head of our global linked list of metadata records
-void* HEAD = NULL;
+// Head and tail of our global linked list of metadata records
+struct metadata* HEAD = NULL;
+struct metadata* TAIL = NULL;
+
+// Find a free block of suitable size in the list, starting from head
+struct metadata* find_free_block(size_t size) {
+    struct metadata* current = HEAD;
+    while (current && !(current->free && current->size >= size)) {
+        current = current->next;
+    }
+    return current;
+}
+
+
+struct metadata* request_space(size_t size) {
+    // New block starts at current end of heap
+    struct metadata* block = sbrk(0);
+    
+    // Extend heap by amount of space required
+    void* request = sbrk(META_SIZE + size);
+    
+    // Return NULL if sbrk failed
+    if (request == (void*) -1) {
+        return NULL;
+    }
+    
+    // Consistency check (race conditions / thread safety)
+    assert ((void*) block == request);
+    
+    // If there is already at least one entry in the list
+    if (TAIL) {
+        //...
+        TAIL->next = block;
+        //...
+        TAIL = block;
+    }
+    
+    // Write entry, and return it
+    block->size = size;
+    block->next = NULL;
+    block->free = 0;
+    return block;
+}
 
 
 void *malloc(size_t size) {
@@ -34,26 +75,23 @@ void *malloc(size_t size) {
     // First call -- our global list is still empty,
     if (!HEAD) {
         // and thus we need to request space from the OS.
-        block = request_space(NULL, size);
+        block = request_space(size);
         
         // Return NULL if request has failed.
         if (!block) {
             return NULL;
         }
         
-        // Otherwise, the requested block becomes our new list head.
+        // Otherwise, the requested block becomes our new list head and tail.
         HEAD = block;
+        TAIL = block;
     
         
     // Not the first call -- there are already blocks on the global list
     } else {
-        // We will search through our list soon, and while doing so,
-        // store the last block of the list here
-        // TODO: This seems kinda optimisable... can't we store a global TAIL?
-        struct metadata* last = NULL;
-        
+
         // Try to find a free block on the list
-        block = find_free_block(&last, size);
+        block = find_free_block(size);
         
         // If we found a suitable free block on the list,
         // mark it as used now.
@@ -62,8 +100,7 @@ void *malloc(size_t size) {
         
         // Else, we need to request more memory from the OS
         } else {
-            // here, we need 'last' to know where to continue our list
-            block = request_space(last, size);
+            block = request_space(size);
             
             // if request failed, we return NULL
             if (!block) {
@@ -77,8 +114,12 @@ void *malloc(size_t size) {
     return (block+1);
 }
 
+void print_list() {
+    // TBD
+    return NULL;
+}
 
 int main() {
-    find_free_block(100);
+    int* x = (int*) malloc(10*sizeof(int));
     return 0;
 }
